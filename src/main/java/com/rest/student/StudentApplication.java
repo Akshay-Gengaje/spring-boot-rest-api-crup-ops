f4c3543a1b415aeae33cde2b1335f6d092bcfa0d
AkshayGengaje <akshay@ai4mtech.com>
2023-05-25 14:27:36 +0530

    added env in application.properties

--- src/main/java/com/rest/student/StudentApplication.java
+++ src/main/java/com/rest/student/StudentApplication.java
@@ -1,6 +1,5 @@
 package com.rest.student;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +8,7 @@ public class StudentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApplication.class, args);
+
 	}
 
 }
